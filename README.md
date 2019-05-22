2ae56c61c4953e11a41ed7f885ded839fd621e25
hakan çelik <hakancelik96@outlook.com>
2019-05-22 19:38:03 +0300

    setup readme

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # github_auth
 A django application to login with github.
+
+### Usage
+
+**/settings.py**
+
+```python
+
+INSTALLED_APPS = [
+    ...
+    ...
+    ...
+    "github_auth",
+]
+
+GITHUB_AUTH = dict(
+    redirect_uri="your_redirect_uri",
+    scope="your scope",
+    client_secret="your github client_secret",
+    client_id="your github client_id",
+)
+
+```
+
+**/urls.py**
+
+```python
+
+urlpatterns = [
+    ...
+    ...
+    ...
+    path("accounts/github/", include('github_auth.urls')),
+]
+
+```
+
+**/templates**
+
+```
+{% url 'redirect-github' %}
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="github_auth",
+    version='0.0.1',
+    description='A django application to login via github.',
+    author='hakancelik96',
+    author_email='hakancelik96@outlook.com',
+    packages=["github_auth"],
+    include_package_data=True,
+    install_requires=[],
+    url="https://github.com/coogger/github_auth",
+    license='MIT',
+    zip_safe=False,
+    keywords="github, django github, login github",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
