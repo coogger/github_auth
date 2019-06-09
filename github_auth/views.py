0e0c83f8de49f9f6969bc22516587b952da1f79d
hakan çelik <hakancelik96@outlook.com>
2019-06-09 14:31:40 +0300

    login bug fixed

--- github_auth/views.py
+++ github_auth/views.py
@@ -18,7 +18,7 @@ github_params = settings.GITHUB_AUTH
 class Github(View):
     access_token_api_url = "https://github.com/login/oauth/access_token"
     user_api_url = "https://api.github.com/user"
-    
+
     def get(self, request, *args, **kwargs):
         code = request.GET.get("code", None)
         if code is None:
@@ -29,29 +29,29 @@ class Github(View):
         if access_token is None:
             raise Http404
         extra_data = requests.get(
-            self.user_api_url, 
+            self.user_api_url,
             headers={"Authorization": f"token {access_token}"}
         )
         extra_data = json.loads(extra_data.text)
         username = extra_data.get("login")
         email = extra_data.get("email")
-        user, created = User.objects.get_or_create(username=username, email=email)
+        user, created = User.objects.get_or_create(username=username)
         if created:
             GithubAuthUser(
-                user=user, 
-                code=code, 
-                access_token=access_token, 
+                user=user,
+                code=code,
+                access_token=access_token,
                 extra_data=extra_data
             ).save()
         else:
-            user.email = email
-            user.save()
             github_user = GithubAuthUser.objects.get(user=user)
-            github_user.code=code, 
-            github_user.access_token=access_token, 
-            github_user.extra_data=extra_data
+            github_user.code = code
+            github_user.access_token = access_token
+            github_user.extra_data = extra_data
             github_user.save()
         login(request, user, backend="django.contrib.auth.backends.ModelBackend")
+        user.email = email
+        user.save()
         return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
 
     @staticmethod
